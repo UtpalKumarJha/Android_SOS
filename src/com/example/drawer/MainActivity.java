13b7e00edcd264598b57350e75209d2df5770090
UtpalKumarJha <coolutpaljha@gmail.com>
2015-02-01 14:10:23 +0530

    Toggle Drawer fixed

--- src/com/example/drawer/MainActivity.java
+++ src/com/example/drawer/MainActivity.java
@@ -40,6 +40,18 @@ public class MainActivity extends ActionBarActivity
     DBADapter db2;
     
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+          return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+    
+    @Override
     protected void onCreate(Bundle savedInstanceState)
     {
     	
